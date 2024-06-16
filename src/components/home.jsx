5a9c27731db1a50b6dd221af60e19724c5572291
aayahda <aadhyakraj@gmail.com>
2024-06-16 20:44:57 +0530

    home added

---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../index.css';
+
+
+function Home(){
+    return <div className="home">
+        <div>
+        <h1>CULTIVATING CHAMPIONS OF TOMORROW</h1>
+        <h3>Unlock your athletic potential with our comprehensive training and development programs.</h3>
+        </div>
+    </div>
+}
+
+export default Home;
