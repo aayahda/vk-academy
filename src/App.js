89b49e5a11e1e12e7ef8089bcb4bc8c6d8a3a7d7
aayahda <aadhyakraj@gmail.com>
2024-06-18 21:16:57 +0530

    statistics added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './components/home';
+import Numbers from './components/numbers';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Numbers/>
     
     </div>
   );
---
+++ src/components/numbers.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function Numbers(){
+    return(<div className="numbers">
+        <div>
+        <h1> WELCOME TO VK FOOTBALL ACADEMY</h1>
+        <h2>Here we develop lorem ipsum dolor sit amet consectetur.</h2>
+        </div>
+        <div className="statistics">
+            <div>
+                <h1>5+</h1>
+                <h3>Coaching centers</h3>
+            </div>
+            <div>
+                <h1>10+</h1>
+                <h3>Experienced coaches</h3>
+            </div>
+            <div>
+                <h1>200+</h1>
+                <h3>Community Members</h3>
+            </div>
+            <div>
+                <h1>600+</h1>
+                <h3>Academy Graduates</h3>
+            </div>
+        </div>
+        
+    </div>);
+}
+
+export default Numbers;
